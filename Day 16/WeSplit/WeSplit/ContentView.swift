5a042f66e3ae0a44ecea99384715aa8d813a65ec
tavvfiq <taufiq.w@mail.ugm.ac.id>
2021-03-08 16:16:09 +0700

    initial upload

---
+++ Day 16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Taufiq Widi on 08/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var numOfPeople: String = ""
+    @State private var tipPercentages: [Int] = [25, 20, 15, 10, 5, 0]
+    @State private var tipPercentage: Int = 0
+    @State private var totalAmount: String = ""
+
+    private var totalPerPerson: Double {
+        let persons = Double(numOfPeople) ?? 2
+        let tip = Double(tipPercentages[tipPercentage])
+        let total = Double(totalAmount) ?? 0
+
+        let tipValue = total*tip/100
+
+        let share = (total + tipValue)/persons
+        return share
+    }
+
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header:Text("amount and people")) {
+                    TextField("Amount", text: $totalAmount)
+                        .keyboardType(.decimalPad)
+
+                    TextField("Number of people", text: $numOfPeople)
+                        .keyboardType(.decimalPad)
+
+                }
+
+                Section(header:Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(0 ..< tipPercentages.count) {
+                                Text("\(self.tipPercentages[$0])%")
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section(header:Text("Total per person")) {
+                    Text("\(totalPerPerson, specifier: "%.0f")")
+                }
+            }
+            .navigationBarTitle(Text("WeSplit"), displayMode: .inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 16/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Taufiq Widi on 08/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
