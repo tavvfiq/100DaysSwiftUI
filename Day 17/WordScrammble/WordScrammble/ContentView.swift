c9814f10a49342f1ccfcbffaf5edf3d60acf8f49
tavvfiq <taufiq.w@mail.ugm.ac.id>
2021-03-10 16:32:00 +0700

    Project 5: ScrambleWord

---
+++ Day 17/WordScrammble/WordScrammble/ContentView.swift
@@ -0,0 +1,127 @@
+//
+//  ContentView.swift
+//  WordScrammble
+//
+//  Created by Taufiq Widi on 10/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    // State
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
+    // Methods
+    private func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        guard isNotUsed(word: newWord) else {
+            wordError(title: "Word entered already", message: "Try another word!")
+            return
+        }
+
+        guard isPossible(word: newWord) else {
+            wordError(title: "Word is not possible", message: "Dont make your own word!")
+            return
+        }
+
+        guard isReal(word: newWord) else {
+            wordError(title: "Word not found", message: "Is that a new language?")
+            return
+        }
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+
+    private func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                let selectedWord = allWords.randomElement() ?? "silkworm"
+                let scrambledWord = selectedWord.components(separatedBy: "").shuffled().joined()
+
+                rootWord = scrambledWord
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+
+    private func isNotUsed(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    private func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+
+    private func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding().autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 17/WordScrammble/WordScrammble/WordScrammbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrammbleApp.swift
+//  WordScrammble
+//
+//  Created by Taufiq Widi on 10/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrammbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
